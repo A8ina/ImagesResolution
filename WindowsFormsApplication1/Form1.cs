3a954cebcf5cc47f9376fb35c95d0152996c0a79
Athina Papadopoulou <a8ina_pap@hotmail.com>
2017-04-07 09:19:18 +0300

    Update Form1.cs
    Ready

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -42,28 +42,35 @@ namespace WindowsFormsApplication1
   
         private void Form1_Load(object sender, EventArgs e)
         {
+        //we take the current date
             DateTime date = DateTime.Now;
+            
             String today = Convert.ToDateTime(date).ToString("yyyy/MM/dd");
+            //change the format in order to store the images with date's name
             String name = Convert.ToDateTime(date).ToString("yyyy_MM_dd_");
             Console.WriteLine(today);
 
             int i = 1;
             bool go = true;
            
-            //test git
+            //we can resize the images according to the resolution we need
 
             Image newImage = Image.FromFile("C:\\2017_03_17_3.jpg");
             ResizeImage(newImage, 2000, 3000);
-
+            
+//supposing we know the pattern where the images are stored in that location eg. test1.jpg, test2.jpg
             //using (WebClient client = new WebClient())
             //{
+            //while the path has images
             //    while (go == true)
             //    {
             //        try
             //        {
-            //            client.DownloadFile("https://digital.philenews.com/data/issues/newspapers/phileleftheros/" + today + "/files/pages/tablet/" + i + ".jpg", "C:\\" + name + i + ".jpg");
+           
+            //            client.DownloadFile("https://www.example.com/test"+i+".jpg", "C:\\" + name +i".jpg");
 
-            //            //client.DownloadFile("https://digital.philenews.com/data/issues/newspapers/phileleftheros/" + today + "/files/pages/tablet/" + i + ".jpg", @"\\192.168.0.100\\Shared Folders\\Users Files\\CyprusNewspapers\\Output\\Phileleutheros\\" + name+ i + ".jpg");
+            //download and store in a network file
+            //            //client.DownloadFile("https://www.example.com/test"+i+".jpg", @"\\xxx.xxx.xxx.xxx\\Shared Folders\\Users Files\\" + name+ i + ".jpg");
             //            i++;
 
             //        }
